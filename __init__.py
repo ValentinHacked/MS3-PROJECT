8db5113550905d0cd1a1befc8c9cb0551f18d8e3
ValentinHacked <HydellRe1@outlook.com>
2022-03-22 20:31:20 +0000

    linked with mongodb

--- __init__.py
+++ __init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 from .extensions import mongo
 
+
 def create_app_():
     app = Flask(__name__)
+
+    app.config['MONGO_URI'] = 'mongodb+srv://ValentinVM:<Password1>@cluster0.qc3we.mongodb.net/mydb?retryWrites=true&w=majority'
 
     mongo.init_app(app)
 
